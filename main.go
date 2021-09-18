3ddb8b774973f380d7cc85fec7d15728baaf50b8
gowseny <gowsy2@gmail.com>
2021-09-18 18:59:56 +0100

    main.go

--- main.go
+++ main.go
@@ -1,131 +1,146 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"github.com/01-edu/z01"
 	"os"
+	"strconv"
+	"strings"
 )
 
-// Sudoko grid is represented as a slice of 81 bytes
-// 0 means number is unknow
-// grid is a global variable, so it is acceisble by main and all functions
-
-var grid [81]byte
-
 func main() {
-	var gridpos byte // variable to hold the current position (0~80) in the grid
+	board := parseInput(os.Args[1])
 
-	args := os.Args[1:]
-	numarg := len(args)
-	if numarg != 9 {
-		fmt.Println("Error")
-		return
+	if backtrack(&board) {
+		fmt.Println("The Sudoku was solved successfully:")
+		printBoard(board)
+	} else {
+		fmt.Printf("The Sudoku can't be solved.")
 	}
+}
 
-	// parse each argument (one row) and fill the grid and test if the grid is valid
-
-	gridpos = 0
-	for i := 0; i < numarg; i++ {
-		srune := []rune(args[i])
-		l := len(srune)
-		if l != 9 {
-			fmt.Println("Error")
-			return
-		}
-		for j := 0; j < l; j++ {
-			var number byte
-			// code to write:
-			// check each character in the row if it is between 1~9 or is .
-			// if character is invalid, exit with error
-			// if character is valid, put the number in the corresponding position of the grid
-			// (put 0 in the grid if character is .)
-
-			// below code still needs to check for invalid characters (other than 1~9 or .)
-
-			if srune[j] != '.' {
-				number = byte(srune[j] - 48)
-				grid[gridpos] = number
-
+func backtrack(board *[9][9]int) bool {
+	if !hasEmptyCell(board) {
+		return true
+	}
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				for candidate := 9; candidate >= 1; candidate-- {
+					board[i][j] = candidate
+					if isBoardValid(board) {
+						if backtrack(board) {
+							return true
+						}
+						board[i][j] = 0
+					} else {
+						board[i][j] = 0
+					}
+				}
+				return false
 			}
+		}
+	}
+	return false
+}
 
-			// after putting each number in the grid, call the Trynum function to check if the grid is valid
-			if !Trynum(gridpos, number) {
-				fmt.Println("Error")
-				return
+func hasEmptyCell(board *[9][9]int) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] == 0 {
+				return true
 			}
-			gridpos++
+		}
+	}
+	return false
+}
+
+func isBoardValid(board *[9][9]int) bool {
+	// check duplicates by row
+	for row := 0; row < 9; row++ {
+		counter := [10]int{}
+		for col := 0; col < 9; col++ {
+			counter[board[row][col]]++
+		}
+		if hasDuplicates(counter) {
+			return false
 		}
 	}
 
-	// Now the grid is filled and checked
-	// so we can solve the sudoku puzzle
+	// check duplicates by column
+	for row := 0; row < 9; row++ {
+		counter := [10]int{}
+		for col := 0; col < 9; col++ {
+			counter[board[col][row]]++
+		}
+		if hasDuplicates(counter) {
+			return false
+		}
+	}
 
-	// write code here to solve the puzzle using the Trynum function
+	// check 3x3 section
+	for i := 0; i < 9; i += 3 {
+		for j := 0; j < 9; j += 3 {
+			counter := [10]int{}
+			for row := i; row < i+3; row++ {
+				for col := j; col < j+3; col++ {
+					counter[board[row][col]]++
+				}
+				if hasDuplicates(counter) {
+					return false
+				}
+			}
+		}
+	}
 
-	// print the grid
-	for gridpos := 0; gridpos < 81; gridpos++ {
-		num := grid[gridpos]
-		if num == 0 {
-			z01.PrintRune('.')
+	return true
+}
+
+func hasDuplicates(counter [10]int) bool {
+	for i, count := range counter {
+		if i == 0 {
+			continue
+		}
+		if count > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func printBoard(board [9][9]int) {
+	fmt.Println("+-------+-------+-------+")
+	for row := 0; row < 9; row++ {
+		fmt.Print("| ")
+		for col := 0; col < 9; col++ {
+			if col == 3 || col == 6 {
+				fmt.Print("| ")
+			}
+			fmt.Printf("%d ", board[row][col])
+			if col == 8 {
+				fmt.Print("|")
+			}
+		}
+		if row == 2 || row == 5 || row == 8 {
+			fmt.Println("\n+-------+-------+-------+")
 		} else {
-			z01.PrintRune(rune(num + '0'))
-		}
-		if (gridpos+1)%9 == 0 {
-			z01.PrintRune('\n')
-		} else {
-			z01.PrintRune(' ')
+			fmt.Println()
 		}
 	}
 }
 
-// fuctions we need
+func parseInput(input string) [9][9]int {
+	board := [9][9]int{}
+	scanner := bufio.NewScanner(strings.NewReader(input))
 
-func Check9(tocheck [9]byte) bool { // check a slice of 9 int for duplicates among 1-9, ignore 0
-	var result bool = true // true = no duplicates,  false = has duplicates
+	scanner.Split(bufio.ScanRunes)
 
-	// code to be written
-
-	return result
-}
-
-func Checkrow(gridpos byte) bool { // check row for duplicates - gridpos (0~80) passed as argument
-	var result bool = true // true = no duplicates,  false = has duplicates
-	var checknums [9]byte
-
-	// code to fill up checknums with the numbers from the row of the griid where the element is
-	// example: checknums[0] = grid[0]
-
-	result = Check9(checknums)
-	return result
-}
-
-func Checkcolumn(gridpos byte) bool { // check column for duplicates - gridpos (0~80) passed as argument
-	var result bool = true // true = no duplicates,  false = has duplicates
-	var checknums [9]byte
-
-	// code to fill up checknums with the numbers from the column
-
-	result = Check9(checknums)
-	return result
-}
-
-func Checkblock(gridpos byte) bool { // check box for duplicates - gridpos (0~80)  passed as argument
-	var result bool = true // true = no duplicates,  false = has duplicates
-	var checknums [9]byte
-
-	// code to fill up checknums with the numbers from the block
-
-	result = Check9(checknums)
-	return result
-}
-
-func Trynum(gridpos byte, numb byte) bool { // check if numb can be placed at gridpos (0~80)
-	var result bool = true // true = number can be placed at gridpos without conflicts, otherwise false
-
-	grid[gridpos] = numb
-	result = Checkrow(gridpos) && Checkcolumn(gridpos) && Checkblock(gridpos)
-	if result == false {
-		grid[gridpos] = 0
+	for row := 0; row < 9; row++ {
+		for col := 0; col < 9; col++ {
+			scanner.Scan()
+			i1, _ := strconv.Atoi(scanner.Text())
+			board[row][col] = i1
+		}
 	}
-	return result
+	return board
 }
